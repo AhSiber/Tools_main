22c0ed0c3b8cf1f73d6eb183d10d3402efd9c507
Ahoora <75671611+AhSiber@users.noreply.github.com>
2021-09-26 17:56:09 +0330

    Update tools_main.py

--- main/tools_main.py
+++ main/tools_main.py
@@ -4,11 +4,16 @@ from pyfiglet import figlet_format
 from socket import gethostbyname, gethostname
 import calendar
 from time import time, localtime
+import wget 
+import requests
 from def_init_mine import my_req
 from define_font import font
 from def_danstny import Danstny
 from def_joke import joke
 from def_numbers import numberss
+from meali import code_meali
+from verify_email import verify_email 
+
 
 # {Knowledge in Persian}
 # Developer : Ahura
@@ -17,17 +22,17 @@ from def_numbers import numberss
 init()  # windows
 
 help_tools = ''' 
-# [1] Dispute 
-# [2] Bank
-# [3] charge
-# [4] ip
-# [5] Date
-# [6] time
-# [7] req
-# [8] font 
-# [9] des
-# [10] joke 
-# [11] Convert numbers from English to Persian  
+# [1] Dispute           [12] craet password 
+# [2] Bank              [13] Wikipedia  
+# [3] charge            [14] Convert text to Morse code  
+# [4] ip                [15] ping site 
+# [5] Date              [16] qr code
+# [6] time              [17] Character  
+# [7] req               [18] True Code Meli 
+# [8] font              [19] Download  
+# [9] des               [20] Email Verification  
+# [10] joke             [21] Developer 
+# [11] number 
 
 '''
 print(Fore.RED + help_tools)
@@ -132,8 +137,98 @@ elif user == "11":
     except:
         print(Fore.RED + "error Tools number ")
 
+elif user == "12" : 
+    try:
+        user_password = int(input('Enter len password : '))
+        name = requests.get(f"http://api.codebazan.ir/password/?length={user_password}").text 
+        print(f"password : {Fore.YELLOW + name}") 
+    except : 
 
-else:
+        print(Fore.RED + "error password") 
+
+
+elif user == "13" : 
+    try: 
+        Wikipedia_user= input('search in Wikipedia : ')
+        Wikipedia = requests.get(f"https://api.codebazan.ir/wiki/?search={Wikipedia_user}").text
+        print(Wikipedia) 
+    except:
+        print(Fore.RED + "error search Wikipedia") 
+
+elif user == "14" : 
+    try : 
+        user_mours = input('type value : ')
+        lange = str(input('enter lange : '))
+        nmours = requests.get(f"https://api.codebazan.ir/mourse/?lang={lange}&text={user_mours}").text
+        print(nmours) 
+    except:
+        print(Fore.RED + "error tools mours")  
+
+elif user == "15" : 
+    try : 
+        user_ping = input('Enter site : ')
+        ping = requests.get(f"https://api.codebazan.ir/ping/?url={user_ping}").text
+        print(f"ping site : {Fore.LIGHTBLUE_EX + ping}")
+
+    except : 
+        print(Fore.RED + "error tools ping")  
+
+
+
+elif user == "16" :
+    try: 
+        sname = input('Enter Address exs QU : ')
+        my = (f"https://api.codebazan.ir/qr/?size=500x500&text={sname}.png") 
+        dow = wget.download(my)  
+        dow = f'{sname}.png'
+        print(dow)
+    except : 
+        print(Fore.RED + "error tools qrCode ")  
+
+elif user == "17" : 
+    try : 
+
+        user_len = input('Enter Character : ')
+        print(F"Number of characters : {len(user_len)}" )
+    except : 
+        print(Fore.RED + "error tools Character ")  
+
+elif user == "18" : 
+    try: 
+        user_code_maily = int(input('Enter Code meli : '))
+        print(code_meali.nano(user_code_maily)) 
+    except: 
+        print(Fore.RED + "error tools codemali")  
+
+elif user == "19" : 
+    try: 
+        users_Download = input('Enter linke : ') 
+        Dosnm = wget.download(users_Download) 
+        print(Dosnm) 
+    except:
+        print(Fore.RED + "error tools Download ")  
+
+
+elif user == "20" : 
+    try: 
+        email = str(input('enter Email : '))
+        print(f"your email {verify_email(email)}")
+    except: 
+        print(Fore.RED + "error tools email ")  
+
+
+elif user == "21" : 
+    new = f''' 
+{Fore.LIGHTCYAN_EX}
+
+            (Knowledge in Persian)
+            Developer : Ahura
+            git hub : https://github.com/AhSiber
+
+    ''' 
+    print(new) 
+
+else:   
     print(Fore.RED + "Error input !")
 
 Time_end = time()
