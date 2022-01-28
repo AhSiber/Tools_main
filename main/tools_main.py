666810b249e27f5080969fe2870788ea0e3d04c0
Ahoora <75671611+AhSiber@users.noreply.github.com>
2022-01-28 17:21:02 +0330

    Update tools_main.py

--- main/tools_main.py
+++ main/tools_main.py
@@ -1,37 +1,40 @@
-import os 
-from colorama import Fore, init # color in python 'colorama' Func in Windows 'init' !
-import gtts # voice in python 
-# from pyfiglet import figlet_format # pyfiglet in python 'Tools' 
-from socket import gethostbyname, gethostname # ip in socket '127.0.0.1'
-import calendar # calend in python '2021' , '4' , '23'
-from time import time, localtime # time 12 : 00 
-import wget # dowanload in python link 
-import requests # request api 
-from def_init_mine import my_req # funck my_req
-from define_font import font # funck font 
-from def_danstny import Danstny # funck danstny 
-from def_joke import joke # funck joke 
-from def_numbers import numberss # funck number 
-from meali import * # funck True code malit 
-from verify_email import verify_email # verfiy Eamil 
-from random import randrange # random number 
-from tkinter import * # GUL 
-import tkinter.messagebox # GUL hanel Error 
-from os import system # os madouls in system 
-from cryptography.fernet import Fernet # Encode end decrypt
+import os
+import pstats
+# color in python 'colorama' Func in Windows 'init' !
+from colorama import Fore, init
+import gtts  # voice in python
+# from pyfiglet import figlet_format # pyfiglet in python 'Tools'
+from socket import gethostbyname, gethostname  # ip in socket '127.0.0.1'
+import calendar  # calend in python '2021' , '4' , '23'
+from time import time, localtime  # time 12 : 00
+import wget  # dowanload in python link
+import requests  # request api
+from def_init_mine import my_req  # funck my_req
+from define_font import font  # funck font
+from def_danstny import Danstny  # funck danstny
+from def_joke import joke  # funck joke
+from def_numbers import numberss  # funck number
+from meali import *  # funck True code malit
+from verify_email import verify_email  # verfiy Eamil
+from random import randrange  # random number
+from tkinter import *  # GUL
+import tkinter.messagebox  # GUL hanel Error
+from os import system  # os madouls in system
+from cryptography.fernet import Fernet  # Encode end decrypt
 import platform
-import sys 
+import sys
+from pytube import YouTube
 
-# clear system ! 
+# clear system !
 os.system("clear")
 
 # {Knowledge in Persian}
 # Developer : Ahura
 # git hub : https://github.com/AhSiber
 
-if sys.platform == "Linux" : 
+if sys.platform == "Linux":
     pass
-else: 
+else:
     init()  # windows
 
 help_tools = ''' 
@@ -39,7 +42,7 @@ help_tools = '''
 # [2] Bank              [13] Wikipedia                      [25] NootBok (GUL)
 # [3] charge            [14] Convert text to Morse code     [26] encode 
 # [4] ip                [15] ping site                      [27] decode 
-# [5] Date              [16] qr code                        [28] shutdown
+# [5] Date              [16] qr code                        [28] Continue...
 # [6] time              [17] Character  
 # [7] req               [18] True Code Meli 
 # [8] font              [19] Download  
@@ -49,23 +52,23 @@ help_tools = '''
                         [23] calculator (GUL)
 
 '''
-print(Fore.RED + help_tools) # color 
+print(Fore.RED + help_tools)  # color
 
 # print(figlet_format('Tools', font="standard")) # Figlet format 'Tools'
-# print(Fore.GREEN) # color green 
-os.system("figlet Tools ")  
+# print(Fore.GREEN) # color green
+os.system("figlet Tools ")
 
 
-user = input('enter Tools : ').lower() # input "lower" -- user 
+user = input('enter Tools : ').lower()  # input "lower" -- user
 if user == "1":
     try:
-        def shop(Overall_profit, Total): # defin inputs (Overall_profit) and (Total)
+        def shop(Overall_profit, Total):  # defin inputs (Overall_profit) and (Total)
             return F"Overall profit : {Overall_profit - Total}"
         Dispute = input('type Overall_profit : ')
         Dispute2 = input('Enter Total : ')
-        print(shop(Dispute, Dispute2)) # print (Overall_profit) and (Total)  
+        print(shop(Dispute, Dispute2))  # print (Overall_profit) and (Total)
     except:
-        print(Fore.RED + "error tools Dispute ") # Handel Error 
+        print(Fore.RED + "error tools Dispute ")  # Handel Error
 
 
 elif user == "2":
@@ -94,78 +97,80 @@ elif user == "3":
 
 elif user == "4":
     try:
-        name = gethostname() # get name host 
-        by_name = gethostbyname(name) # get host by name 
+        name = gethostname()  # get name host
+        by_name = gethostbyname(name)  # get host by name
         os.system("clear")
-        print(f"ip address : ", Fore.RED + by_name) # red ip 127.0.0.1
+        print(f"ip address : ", Fore.RED + by_name)  # red ip 127.0.0.1
     except:
-        print(Fore.RED + "error tools host ") # handel Error 
+        print(Fore.RED + "error tools host ")  # handel Error
 
 
 elif user == "5":
     try:
         user_clender = int(input('Enter Yers : '))
         user_month = int(input('type manth : '))
-        new_s_f = calendar.month(user_clender, user_month) # clander 2021 
+        new_s_f = calendar.month(user_clender, user_month)  # clander 2021
         os.system("clear")
-        print(Fore.RED + new_s_f) # color red 
+        print(Fore.RED + new_s_f)  # color red
     except:
-        print(Fore.RED + "error Tools calender ") # handel error 
+        print(Fore.RED + "error Tools calender ")  # handel error
 
 
 elif user == "6":
     try:
-        user_time = time() # time [272.9293827382] 
-        my_time = localtime(user_time) # local time tm_hour or tm_main or tm_sec 
+        user_time = time()  # time [272.9293827382]
+        # local time tm_hour or tm_main or tm_sec
+        my_time = localtime(user_time)
         os.system("clear")
-        print(F"{my_time.tm_hour}:{my_time.tm_min}:{my_time.tm_sec}") 
+        print(F"{my_time.tm_hour}:{my_time.tm_min}:{my_time.tm_sec}")
     except:
-        print(Fore.RED + "error tools time ") # handel Error 
+        print(Fore.RED + "error tools time ")  # handel Error
 
 elif user == "7":
     try:
         os.system('clear')
-        print(my_req.req()) # req
+        print(my_req.req())  # req
     except:
-        print(Fore.RED + 'error tools req') # handel eror 
+        print(Fore.RED + 'error tools req')  # handel eror
 
 
 elif user == "8":
     try:
         os.system("clear")
-        font.font_en() # font =>  60 
+        font.font_en()  # font =>  60
     except:
-        print(Fore.RED + "eror tools font ") # handel Error 
+        print(Fore.RED + "eror tools font ")  # handel Error
 
 
 elif user == "9":
     try:
         os.system("clear")
-        print(Danstny.dnst()) # danstny funck == 5000 == 
+        print(Danstny.dnst())  # danstny funck == 5000 ==
     except:
-        print(Fore.RED + "error tools des ") # handel Error 
+        print(Fore.RED + "error tools des ")  # handel Error
 
 elif user == "10":
     try:
         os.system('clear')
-        print(joke.jokes()) # jocke == 7000 === 
+        print(joke.jokes())  # jocke == 7000 ===
     except:
         print(Fore.RED + "error tools joke")
 
 
 elif user == "11":
     try:
-        print(numberss.number()) # exchange number in str 
+        print(numberss.number())  # exchange number in str
     except:
-        print(Fore.RED + "error Tools number ") # handel error 
+        print(Fore.RED + "error Tools number ")  # handel error
 
 elif user == "12":
     try:
-        user_password = int(input('Enter len password : ')) # input len password 
+        user_password = int(input('Enter len password : ')
+                            )  # input len password
         name = requests.get(
-            f"http://api.codebazan.ir/password/?length={user_password}").text # api Create password 
+            f"http://api.codebazan.ir/password/?length={user_password}").text  # api Create password
         os.system("clear")
-        print(f"password : {Fore.YELLOW + name}") 
+        print(f"password : {Fore.YELLOW + name}")
     except:
 
         print(Fore.RED + "error password")
@@ -173,9 +178,9 @@ elif user == "12":
 
 elif user == "13":
     try:
-        Wikipedia_user = input('search in Wikipedia : ') # input serach 
+        Wikipedia_user = input('search in Wikipedia : ')  # input serach
         Wikipedia = requests.get(
-            f"https://api.codebazan.ir/wiki/?search={Wikipedia_user}").text # api seach in wikipadia 
+            f"https://api.codebazan.ir/wiki/?search={Wikipedia_user}").text  # api seach in wikipadia
         os.system("clear")
         print(Wikipedia)
     except:
@@ -183,19 +188,19 @@ elif user == "13":
 
 elif user == "14":
     try:
-        user_mours = input('type value : ') # str 
-        lange = str(input('enter lange : ')) # lange 
+        user_mours = input('type value : ')  # str
+        lange = str(input('enter lange : '))  # lange
         nmours = requests.get(
-            f"https://api.codebazan.ir/mourse/?lang={lange}&text={user_mours}").text # mours 
+            f"https://api.codebazan.ir/mourse/?lang={lange}&text={user_mours}").text  # mours
         print(nmours)
     except:
         print(Fore.RED + "error tools mours")
 
 elif user == "15":
     try:
-        user_ping = input('Enter site : ') # input site 
+        user_ping = input('Enter site : ')  # input site
         ping = requests.get(
-            f"https://api.codebazan.ir/ping/?url={user_ping}").text # ping site 
+            f"https://api.codebazan.ir/ping/?url={user_ping}").text  # ping site
         os.system("clear")
         print(f"ping site : {Fore.LIGHTBLUE_EX + ping}")
 
@@ -205,33 +210,35 @@ elif user == "15":
 
 elif user == "16":
     try:
-        sname = input('Enter Address exs QR : ') # iter code in QR code 
-        my = (f"https://api.codebazan.ir/qr/?size=500x500&text={sname}.png") # api code RQ
-        dow = wget.download(my) # download QR 
-        dow = f'{sname}.png' # format '.png'
+        sname = input('Enter Address exs QR : ')  # iter code in QR code
+        # api code RQ
+        my = (f"https://api.codebazan.ir/qr/?size=500x500&text={sname}.png")
+        dow = wget.download(my)  # download QR
+        dow = f'{sname}.png'  # format '.png'
         print(dow)
     except:
-        print(Fore.RED + "error tools qrCode ") # handel Error 
+        print(Fore.RED + "error tools qrCode ")  # handel Error
 
 elif user == "17":
     try:
-        user_len = input('Enter Character : ') # enter charchter === name == len(4) 
+        # enter charchter === name == len(4)
+        user_len = input('Enter Character : ')
         os.system("clear")
-        print(F"Number of characters : {len(user_len)}") 
+        print(F"Number of characters : {len(user_len)}")
     except:
         print(Fore.RED + "error tools Character ")
 
 elif user == "18":
     try:
         user_code_maily = input('Enter Code meli : ')
-        print(code_meali.mali(user_code_maily)) # funck code True end False 
+        print(code_meali.mali(user_code_maily))  # funck code True end False
     except:
         print(Fore.RED + "error tools codemali")
 
 elif user == "19":
     try:
-        users_Download = input('Enter linke : ') # enter linke 
-        Dosnm = wget.download(users_Download) # download linke inputy !  
+        users_Download = input('Enter linke : ')  # enter linke
+        Dosnm = wget.download(users_Download)  # download linke inputy !
         os.system("clear")
         print(Dosnm)
     except:
@@ -240,14 +247,14 @@ elif user == "19":
 
 elif user == "20":
     try:
-        email = str(input('enter Email : ')) # Enter email 
+        email = str(input('enter Email : '))  # Enter email
         os.system("clear")
-        print(f"your email {verify_email(email)}") # verfiy Emalit 
+        print(f"your email {verify_email(email)}")  # verfiy Emalit
     except:
         print(Fore.RED + "error tools email ")
 
 
-elif user == "21": # Developer 
+elif user == "21":  # Developer
     num = Tk()
     num.title("Developer")
     num.geometry("300x200")
@@ -387,13 +394,13 @@ elif user == "23":
 
 elif user == "24":
     try:
-        en_link_addres = input('Enter linke site : ') # linke DDos 
-        new_my = system(f"ping {en_link_addres}") # ddos in Terminall  
+        en_link_addres = input('Enter linke site : ')  # linke DDos
+        new_my = system(f"ping {en_link_addres}")  # ddos in Terminall
     except:
         print(Fore.RED + "error tools DDos ")
 
 
-elif user == "25": # notbook 
+elif user == "25":  # notbook
     root = Tk()
     root.geometry("180x120")
     root.resizable(width=False, height=False)
@@ -411,83 +418,131 @@ elif user == "25": # notbook
 
 
 elif user == "26":
-    Crete = Fernet.generate_key() # Crear key  
-    print(f"key :  {Crete} ") # print 'key'
-    user_enc = input('Enter path : ') # input path
+    Crete = Fernet.generate_key()  # Crear key
+    print(f"key :  {Crete} ")  # print 'key'
+    user_enc = input('Enter path : ')  # input path
 
-    with open(user_enc, "rb") as f: # read in file  'user_enc' 
-        date = f.read() # read 
+    with open(user_enc, "rb") as f:  # read in file  'user_enc'
+        date = f.read()  # read
 
-    nameStr = str(randrange(1, 100000000)) # name file_2 
+    nameStr = str(randrange(1, 100000000))  # name file_2
 
-    file_2 = open(nameStr, "wb") # open To file 
+    file_2 = open(nameStr, "wb")  # open To file
 
-    encd = Fernet(Crete) 
+    encd = Fernet(Crete)
 
-    mnt = encd.encrypt(date) #encrypt file 
-    file_2.write(mnt) # write file encrpte 
+    mnt = encd.encrypt(date)  # encrypt file
+    file_2.write(mnt)  # write file encrpte
 
 
-# ================================================ dec ============================= 
+# ================================================ dec =============================
 elif user == "27":
-    key = Fernet.generate_key() 
+    key = Fernet.generate_key()
 
     user_filw = input('Enter patch : ')
-    filew = open(user_filw , 'rb')  
-    data_10 = filew.read()  
+    filew = open(user_filw, 'rb')
+    data_10 = filew.read()
 
-    filew.close() 
+    filew.close()
 
     user_key = str(input("Enter key : "))
-    f = Fernet(user_key) 
-    name_str = str(randrange(1,100000))
-    files = open(f'{name_str}.png' , "wb")
+    f = Fernet(user_key)
+    name_str = str(randrange(1, 100000))
+    files = open(f'{name_str}.png', "wb")
 
-    enc_2 = f.decrypt(data_10) 
-    files.write(enc_2) 
-
-
-#shut down =========================================================== 
-
-elif user == "28" : 
-    try : 
-        plat = platform.system() 
-        print(plat)
-        if plat == "Windows" : 
-            system('shutdown -s -t 10')
-
-        elif plat == "linux" : 
-            system('shutdown -h 10 ') 
-
-        elif plat == "darwin"  :
-            system("shutdown -h -t 10 ") 
-    except : 
-        print(Fore.RED + "error tools shutdown ")  
-
-else:
-    print(Fore.RED + "Error input !")
+    enc_2 = f.decrypt(data_10)
+    files.write(enc_2)
 
 
+# shut down ===========================================================
+
+elif user == '28':
+    # clear page
+    clears_ = system("clear")
+    # page 1 main
+    page_1 = """
+# [29] shutdown
+# [30] show network - connected and disconnected
+# [31] show UUID 
+# [32] ipconfig
+# [33] YouTube Downloads
+
+    """
+    print(Fore.GREEN + page_1)
+    user_page_1 = int(input('enter tools : ').lower())  # input tools number
+
+    # shutdown system
+    if user_page_1 == 29:
+        plat = platform.system()
+        try:
+            if plat == "Windows":
+                system('shutdown -s -t 10')
+
+            elif plat == "linux":
+                system('shutdown -h 10 ')
+
+            elif plat == "darwin":
+                system("shutdown -h -t 10 ")
+        except:
+            print(Fore.RED + "error tools shutdown ")
+
+    elif user_page_1 == 30 : 
+        if platform.system() == "Linux":
+            nmcli_dev = system("nmcli dev") 
+            # show DEVICE  TYPE  STATE  CONNECTION
+        elif platform.system() == "Windows": 
+            net_use = system("net use")
+        else: 
+            print("❌support") 
+    
+    elif user_page_1 == 31: 
+        # show network co...
+        if platform.system() == "Linux": 
+            nmcli_con = system("nmcli con") 
+        else: 
+            pass
+    
+    elif user_page_1 == 32: 
+        # show network
+        if platform.system() == "Linux":
+            ifconifg = system("ifconfig")
+
+        elif platform.system() == "Windows":
+            ipconifg = system("ipconfig") 
+        else: 
+            pass
+    
+    elif user_page_1 == 33: 
+        youtube_download = str(input('Enter Link Youtube : ')) 
+        # https://youtu.be/prlaBaJm0rU
+        try:
+            video =YouTube(url=youtube_download) 
+            streem = video.streams.get_highest_resolution()
+            streem.download()
+        except: 
+            print(Fore.RED + "error download link " + Fore.BLUE + f"[{youtube_download}]")
+    
 # ======================================================= Time =======
-Time_end_res = time() # time 
-my_and_rews = localtime(Time_end_res)  
+Time_end_res = time()  # time
+my_and_rews = localtime(Time_end_res)
 
 De_time = (f"{my_and_rews.tm_hour}:{my_and_rews.tm_min}:{my_and_rews.tm_sec}")
 
 
-# =========================================== Date =========================  
-Time_end = time() # time 
+# =========================================== Date =========================
+Time_end = time()  # time
 my_and_time = localtime(Time_end)
-with open("./with.txt", mode='a') as files: # write to txt 
-    files.write(F"\nTools : {user} \ntime : {De_time} \nplatform : {platform.system()}") # Date
+with open("./with.txt", mode='a') as files:  # write to txt
+    files.write(
+        F"\nTools : {user} \ntime : {De_time} \nplatform : {platform.system()}")  # Date
     files.write("\n--------------------")
 
-# =========================================== Date =========================  
+# =========================================== Date =========================
 
-if user == '19' : 
-    with open("./with.txt", mode='a') as files: # write to txt 
-        files.write(F"\nTools : {user} \ntime : {De_time} \nplatform : {platform.system()} \nlink : {users_Download}") # Date
+if user == '19':
+    with open("./with.txt", mode='a') as files:  # write to txt
+        files.write(
+            F"\nTools : {user} \ntime : {De_time} \nplatform : {platform.system()} \nlink : {users_Download}")  # Date
         files.write("\n--------------------")
-else : 
+else:
     exit()
-
